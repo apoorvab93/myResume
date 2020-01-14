5e943fa168ad2504d4d279262c16bf90342b3a11
Apoorva <bhatnagar.apoorva93@gmail.com>
2020-01-14 13:50:59 -0800

    Updated code

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace Resume
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
